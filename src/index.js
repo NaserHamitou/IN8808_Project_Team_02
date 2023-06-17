a46c14661e917c21b9b755112be74d50830999ea
OuLour <49345524+Lourmati@users.noreply.github.com>
2023-06-17 18:54:57 -0400

    structure

--- src/index.js
+++ src/index.js
@@ -1,5 +1,43 @@
 'use strict'
 
-import * as home from './scripts/home.js'
+import * as helper from './scripts/helper.js'
+import * as preproc from './scripts/preprocess.js'
+import * as viz from './scripts/viz.js'
+import * as legend from './scripts/legend.js'
+import * as hover from './scripts/hover.js'
+import * as util from './scripts/util.js'
 
-import d3Tip from 'd3-tip'
+import * as d3Chromatic from 'd3-scale-chromatic'
+
+/**
+ * @file This file is the entry-point for the the code for the project for the course INF8808.
+ * @author Olivia Gélinas
+ * @version v1.0.0
+ */
+
+(function (d3) {
+  
+
+  d3.csv('./arbres.csv', d3.autoType).then(function (data) {
+   
+
+    /**
+     *   This function handles the graph's sizing.
+     */
+    function setSizing () {
+      
+    }
+
+    /**
+     *   This function builds the graph.
+     */
+    function build () {
+      
+    }
+
+    window.addEventListener('resize', () => {
+      setSizing()
+      build()
+    })
+  })
+})(d3)
