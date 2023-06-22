865e286e07568494a038ac655640b4d754a6e879
OuLour <49345524+Lourmati@users.noreply.github.com>
2023-06-22 17:27:18 -0400

    Visualisation 4 : Add files

--- src/index.js
+++ src/index.js
@@ -1,38 +1,77 @@
 'use strict'
 
-import * as helper from './scripts/helpers/helper.js'
-import * as preproc from './scripts/preprocess.js'
-// import * as viz from './scripts/viz.js'
-// import * as legend from './scripts/legend.js'
-// import * as hover from './scripts/hover.js'
-// import * as util from './scripts/util.js'
-
+import * as helper from './scripts/helper.js'
+import * as viz from './scripts/viz.js'
+import * as legend from './scripts/legend.js'
+import * as hover from './scripts/hover.js'
 import * as d3Chromatic from 'd3-scale-chromatic'
 
 /**
- * @file This file is the entry-point for the the code for the project for the course INF8808.
- * @author Olivia Gélinas
- * @version v1.0.0
+ * @file This file is the entry-point for the the code of the performance heatmap.
  */
 
 (function (d3) {
-  
+  let bounds
+  let svgSize
+  let graphSize
 
-  d3.csv('./arbres.csv', d3.autoType).then(function (data) {
-   
+  const margin = { top: 35, right: 300, bottom: 35, left: 300 }
+
+  const xScale = d3.scaleBand().padding(0.025)
+  const yScale = d3.scaleBand().padding(0.1)
+  const colorScale = d3.scaleSequential(d3Chromatic.interpolateGreens)
+
+  d3.csv('./Performance.csv', d3.autoType).then(function (data) {
+    const opponents = data.map(d => { return d.Adversaire })
+
+    viz.setColorScaleDomain(colorScale)
+
+    legend.initGradient(colorScale)
+    legend.initLegendBar()
+    legend.initLegendAxis()
+
+    const g = helper.generateG(margin)
+
+    helper.appendAxes(g)
+    viz.appendRects(data, xScale, yScale, colorScale)
+
+    setSizing()
+    build()
 
     /**
      *   This function handles the graph's sizing.
      */
     function setSizing () {
-      
+      bounds = d3.select('.graph').node().getBoundingClientRect()
+
+      svgSize = {
+        width: bounds.width,
+        height: 550
+      }
+
+      graphSize = {
+        width: svgSize.width - margin.right - margin.left,
+        height: svgSize.height - margin.bottom - margin.top
+      }
+
+      helper.setCanvasSize(svgSize.width, svgSize.height)
     }
 
     /**
      *   This function builds the graph.
      */
     function build () {
-      
+      viz.updateXScale(xScale, data, graphSize.width)
+      viz.updateYScale(yScale, opponents, graphSize.height)
+
+      viz.drawXAxis(xScale)
+      viz.drawYAxis(yScale, graphSize.width)
+
+      viz.updateRects(xScale, yScale, data, colorScale)
+
+      hover.setRectHandler(xScale, yScale, hover.rectSelected, hover.rectUnselected, hover.selectTicks, hover.unselectTicks)
+
+      legend.draw(margin.left / 1.5, margin.top + 5, graphSize.height - 10, 25, 'url(#gradient)', colorScale)
     }
 
     window.addEventListener('resize', () => {
