ff686a4e3dc915935af2c355098af20dde5b4cd8
OuLour <49345524+Lourmati@users.noreply.github.com>
2023-06-24 02:00:13 -0400

    viz6 : cleaned

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import * as performance from './scripts/viz4/build.js'
 import * as results from './scripts/viz1/build.js'
+import * as offensive from './scripts/viz6/build.js'
 
 performance.build()
 results.build()
+offensive.build()
