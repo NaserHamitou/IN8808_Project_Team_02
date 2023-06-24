6bd015cbf1aa5bb7e3be7d6b2ca783369898ac28
OuLour <49345524+Lourmati@users.noreply.github.com>
2023-06-24 02:19:17 -0400

    viz3 : cleaned

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import * as performance from './scripts/viz4/build.js'
 import * as results from './scripts/viz1/build.js'
 import * as offensive from './scripts/viz6/build.js'
+import * as cards from './scripts/viz3/build.js'
 
-performance.build()
-results.build()
-offensive.build()
+
+results.build() // Viz 1
+cards.build() // Viz 3
+performance.build() // Viz 4
+offensive.build() // Viz 6
