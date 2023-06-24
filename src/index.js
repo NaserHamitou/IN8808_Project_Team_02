03fc03e9564b1d5d3e26bfeee8cd41c07fe1dc01
NaserHamitou <nas.hmt44@gmail.com>
2023-06-24 15:01:30 -0400

    fix viz3

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import * as performance from './scripts/viz4/build.js'
 import * as results from './scripts/viz1/build.js'
 import * as offensive from './scripts/viz6/build.js'
-import * as cards from './scripts/viz3/build.js'
+import * as cards from './scripts/viz3-cartons/cartons.js'
 
 
 results.build() // Viz 1
-cards.build() // Viz 3
+cards.build()
 performance.build() // Viz 4
 offensive.build() // Viz 6
